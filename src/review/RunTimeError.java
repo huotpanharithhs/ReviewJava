77e58620a6d282ef3b6e05efc299132199601d89
huotpanharithhs <72050103+huotpanharithhs@users.noreply.github.com>
2021-07-11 15:29:34 +0700

    commit class run time error.

---
+++ src/review/RunTimeError.java
@@ -0,0 +1,13 @@
+package review;
+
+import java.util.Scanner;
+
+public class RunTimeError {
+    public static void main(String[] args) {
+        System.out.println(5/0);
+//        int a ;
+//        System.out.print("Enter value a:");
+//        a = new Scanner(System.in).nextInt();
+//        System.out.println((double)5/a);
+    }
+}
